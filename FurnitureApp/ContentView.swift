c10b912f83fa5b50476a3e87804f47e89c534792
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-08-15 09:51:01 +0300

    Initial Commit

---
+++ FurnitureApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FurnitureApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 15.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
